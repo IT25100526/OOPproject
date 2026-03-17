e99e6e9a599d682f423e87ba83d1a7f3e87731f9
IT25100526 <it25100526@my.sliit.lk>
2026-03-17 10:03:11 +0530

    update

--- src/main/java/com/drivingschool/util/DBConnection.java
+++ src/main/java/com/drivingschool/util/DBConnection.java
@@ -12,7 +12,7 @@ public class DBConnection {
 
     private static final String URL      = "jdbc:mysql://localhost:3306/driving_school_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER     = "root";
-    private static final String PASSWORD = "12345678";
+    private static final String PASSWORD = "";
 
     static {
         try {
