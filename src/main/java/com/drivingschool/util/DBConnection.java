34b3b2336d1109dc7cc864c6148c0cc1e5513654
IT25100526 <it25100526@my.sliit.lk>
2026-03-18 07:22:33 +0530

    update
    dp at to online db

--- src/main/java/com/drivingschool/util/DBConnection.java
+++ src/main/java/com/drivingschool/util/DBConnection.java
@@ -5,14 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- * Database connection utility.
- * Reads credentials from application.properties via System properties.
+ * Database connection utility – Aiven Cloud MySQL
  */
 public class DBConnection {
 
-    private static final String URL      = "jdbc:mysql://localhost:3306/driving_school_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String USER     = "root";
-    private static final String PASSWORD = "";
+    private static final String HOST     = "drivingchooldb-my-4536.a.aivencloud.com";
+    private static final String PORT     = "16760";
+    private static final String DATABASE = "driving_school_db";
+    private static final String USER     = "avnadmin";
+    private static final String PASSWORD = "AVNS_rOC9kStdSBH-EqIfTpr";
+
+    private static final String URL =
+            "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE +
+                    "?useSSL=true" +
+                    "&requireSSL=true" +
+                    "&sslMode=REQUIRED" +
+                    "&verifyServerCertificate=false" +
+                    "&allowPublicKeyRetrieval=true" +
+                    "&serverTimezone=UTC" +
+                    "&useUnicode=true" +
+                    "&characterEncoding=UTF-8";
 
     static {
         try {
